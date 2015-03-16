62b10d8619967471dcfb2b4808657bc402d9671f
Mohamad Hatta <hatta91@users.noreply.github.com>
2015-03-16 18:13:29 +0800

    Update index.php
    update

--- application/views/index.php
+++ application/views/index.php
@@ -1,16 +1,43 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	
 	<title>Sistem Pengurusan File</title>
+<style type="text/css">
 
+
+#top{width:1300px; height:120px; background-color: #2D9C92; border-top-left-radius:10px;border-top-right-radius:10px;}
+#undermenu{width:1300px;height:19px;background-color: #FFF;}
+#menu{width:1300px;height:35px;background-color:#000}
+#wrap {width:1300px; background-color:#2D9C92;}
+.submenu{width:200px;height:100px; background-color: #FFF; float:left;}
+.middle{width:1100px;height:auto;background-color: #FFF; float:right;}
+#bottom{width:1300px;height:20px;background-color: #2D9C92; border-bottom-left-radius:10px;border-bottom-right-radius:10px; clear:both;}
+</style>
 	
 </head>
 <body>
-<center>
-<BR><BR><BR><BR><BR><BR>
-<A href="<?php echo base_url('index.php/upload/files')?>">
-<center><input type="button" value="View All Upload Files" /></center> </A>
+<BR>
+<div id="top" style="margin:0 auto"></div>
+
+<div id="menu" style="margin:0 auto"><?php include('menu.php');?>
+</div>
+<div id="undermenu" style="margin:0 auto">
+</div>
+<div id="wrap" style="margin:0 auto">
+
+
+   <div class="submenu" style="margin:0 auto"><BR /><BR />
+
+   </div>
+
+   <div class="middle" style="margin:0 auto"><BR /><BR />
+     
+     <center>
+     <A href="<?php echo base_url('upload/files')?>">
+<input type="button" value="View All Upload Files" /> </A>
 <BR><BR><BR>
  <form action="<?php echo site_url('upload/upload_file'); ?>" method="post" enctype="multipart/form-data">
  <table width="422">
@@ -41,7 +68,22 @@
  </tr>
  </table>
  </form>
+ </center>
+   </div>
+
+</div>
+
+<div id="bottom" style="margin:0 auto"></div>
+
+<BR />
+
+<center>
+<FONT face="Verdana, Geneva, sans-serif" size="-5" color="#333333">Copyright Reserve &copy; 2015 Tuffah Informatics <BR /><BR />
+
 
 </center>
+
+
+
 </body>
 </html>
